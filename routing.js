12e9f109347e664c49c22853a99d6e61966bd938
summit <capitalmarkettools.org@gmail.com>
2015-08-22 14:04:36 -0400

    Almost initial version

---
+++ routing.js
@@ -0,0 +1,15 @@
+/**
+ * Created by cmt on 8/22/15.
+ */
+Router.configure({
+    layoutTemplate: 'main'
+});
+
+Router.route('/', {
+    template: 'home'
+});
+
+Router.route('/home');
+
+Router.route('/about');
+
