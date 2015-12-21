9a51ede4e5a8748a9f61c0d7cabaac70b40fa212
capitalmarkettools <capitalmarkettools.org@gmail.com>
2015-12-20 22:13:16 -0500

    Interim changes in game development to be able to checkout on ios and work there.

--- client/events.js
+++ client/events.js
@@ -39,7 +39,9 @@ Template.rules.events({
     'submit .btcAddress': function(event){
         //Prevent default browser form submit
         event.preventDefault();
+
         Meteor.call('setBTCAddress', event.target.btcAddress.value);
+
         Router.go('/home');
     }
 });
@@ -64,10 +66,8 @@ Template.newGame.events({
         // Prevent default browser form submit
         event.preventDefault();
 
-        var btcAmount = event.target.btcAmount.value;
+        Meteor.call('addGame', event.target.btcAmount.value);
 
-//        alert(btcAmount);
-        Meteor.call('addGame', btcAmount);
         Router.go('/listGames');
     }
 });
@@ -88,15 +88,23 @@ Template.listGames.events({
     'click #practiceGame': function () {
         Router.go('/game');
     },
-    'click #joinGame': function(event){
+    'click #joinGame': function(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+
         /* Algorithm is as follows:
-        1. Get BTC Amount which defines the game (for active games; completed games should be ignored)
-        2. Use URI to send BTC to game bitcoin address
-        3. Find game in Mongo and update: In Progress
+         1. Get BTC Amount which defines the game (for active games; completed games should be ignored)
+         2. Use URI to send BTC to game bitcoin address
+         3. Find game in Mongo and update: In Progress
+         window.location = "bitcoin:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?amount=20.3&label=Luke-Jr"
          */
-        window.location = "bitcoin:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?amount=20.3&label=Luke-Jr"
+        //console.log('In click listGames.#joinGame**********')
+
+        Meteor.call('joinGame', this);
+        //console.log('Out click listGames.#joinGame**********')
     }
 });
+
 
 /*
 Template.about.events({
