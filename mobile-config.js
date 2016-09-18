b358e034b3642ec4a0d68b4b98b796bb138c12f3
capitalmarkettools <capitalmarkettools.org@gmail.com>
2016-09-18 19:35:50 -0400

    Added btcaddress from coinbase when generating game. Added packages to package.json. Add node_modules directory to .gitignore. Install requires to do 'npm install'. Small fixed in mobile-config.js.

--- mobile-config.js
+++ mobile-config.js
@@ -3,8 +3,8 @@ App.info({
   description: 'A skill based game to win/loose BTC.',
   author: 'CapitalMarketTools',
   email: 'tim@capitalmarkettools.com',
-  website: 'http://pressit.capitalmarkettools.org.meteor.com',
-  version: '1.0.0'
+  website: 'https://pressit.capitalmarkettools.org',
+  version: '0.0.1'
 });
 
 //TODO: Review all of these
@@ -27,8 +27,6 @@ App.launchScreens({
   'iphone_2x': 'resources/splash/Default@2x~iphone_640x960.png',
   'iphone5': 'resources/splash/Default-568h@2x~iphone_640x1136.png',
   'iphone6': 'resources/splash/Default-1242@3x~iphone6s-landscape_2208x1242.png',
-//  'iphone6_portrait': 'resources/splash/Default-1242@3x~iphone6s-portrait_1242x2208.png',
-//  'iphone6_landscape': 'resources/splash/Default-1242@3x~iphone6s-landscape_2208x1242.png',
   'ipad_portrait': 'resources/splash/Default~iphone.png',
   'ipad_landscape': 'resources/splash/Default~iphone.png',
   'ipad_portrait_2x': 'resources/splash/Default~iphone.png',
